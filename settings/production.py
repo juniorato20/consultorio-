6d196d5aa49a2297859142d2bc1fffd8e4934634
jhunniorgt <jhunniorguerrero@gmail.com>
2021-05-04 22:16:50 -0500

    inicio de heroku de dominio correcion

--- settings/production.py
+++ settings/production.py
@@ -12,11 +12,11 @@ ALLOWED_HOSTS = ['consultorioodontologico.herokuapp.com']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'citas',
-        'USER' : 'postgres',
-        'PASSWORD' : '12345',
-        'HOST' : '127.0.0.1',
-        'DATABASE_PORT': '5432',
+        'NAME': 'ddtlvkaigh28va',
+        'USER' : 'wksaantkstodfe',
+        'PASSWORD' : 'afe49a3b46b439f0735d7ac2f3c152f39e0d4296cc077e246d541383b3a09183',
+        'HOST' : 'ec2-54-163-254-204.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
