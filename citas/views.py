aad6f97d210950cbd158397cb4accb5789aa1abf
jhunniorgt <jhunniorguerrero@gmail.com>
2021-05-07 11:31:25 -0500

    añadidodj_static y modificadoel arcchivowsgi

--- citas/views.py
+++ citas/views.py
@@ -110,7 +110,7 @@ class CrearCita(SuccessMessageMixin,CreateView):
 
 class ListadoCita(ListView):
     template_name= 'cita/listar_cita.html'
-    queryset = Citas.objects.all().order_by('-id')
+    queryset = Cita.objects.all().order_by('-id')
     paginate_by = 3
     context_object_name = 'citas'
 
