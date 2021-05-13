97c5c3498c120e06ac47ef753513e077b42aeca2
jhunniorgt <jhunniorguerrero@gmail.com>
2021-05-13 15:41:28 -0500

    añadidodj_static y modificadoel arcchivowsgi

--- citas/admin.py
+++ citas/admin.py
@@ -8,7 +8,7 @@ class PacienteResource(resources.ModelResource):
        model = Paciente
 
 class PacienteAdmin(ImportExportModelAdmin):
-    list_display = ('nombre','apellido','direccion','sexo','correo','celular')
+    list_display = ('nombre','apellido','direccion','sexo','correo','celular','estado')
     search_fields = ['nombre', 'apellido']
     resources_class = PacienteResource
 
