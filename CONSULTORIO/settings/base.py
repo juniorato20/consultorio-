576dc61f1ec5a9acc9e8291d20f89fb01ad2c421
jhunniorgt <jhunniorguerrero@gmail.com>
2021-05-15 22:03:09 -0500

    añadidodj_static y modificadoel arcchivowsgi

--- CONSULTORIO/settings/base.py
+++ CONSULTORIO/settings/base.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 from pathlib import Path
 import os
-from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'buvk)-5e6wdf9^n33@&y9z%^t1h8y&a62nyog0x=@$^@iswzd5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'citas',
+
     'widget_tweaks',
     'django_crontab',
     
