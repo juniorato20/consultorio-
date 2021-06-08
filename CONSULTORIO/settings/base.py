954b2b776183397c87b1ce9aaceb7ee2b2b4413c
jhunniorgt <jhunniorguerrero@gmail.com>
2021-06-08 11:26:46 -0500

    añadidodj_static y modificadoel arcchivowsgi

--- CONSULTORIO/settings/base.py
+++ CONSULTORIO/settings/base.py
@@ -126,13 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/')
-]
-
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 EMAIL_USE_TLS = True
--- CONSULTORIO/settings/local.py
+++ CONSULTORIO/settings/local.py
@@ -1,10 +1,8 @@
 from .base import *
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
